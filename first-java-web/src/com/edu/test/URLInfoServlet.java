3ced24596f82207757d8ddd438807eadc15c1eba
engyjoon <bly99@naver.com>
2017-10-06 23:41:56 +0900

    chapter-03 완료

---
+++ first-java-web/src/com/edu/test/URLInfoServlet.java
@@ -0,0 +1,30 @@
+package com.edu.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/urlInfo")
+public class URLInfoServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=EUC-KR");
+		PrintWriter out = resp.getWriter();
+		out.print("<html>");
+		out.print("<head><title>Request 정보 출력 Servlet</title></head>");
+		out.print("Request URI : " + req.getRequestURI() + "<br/>");
+		out.print("Request URL : " + req.getRequestURL() + "<br/>");
+		out.print("Context Path : " + req.getContextPath() + "<br/>");
+		out.print("Request Protocol : " + req.getProtocol() + "<br/>");
+		out.print("Servlet Path : " + req.getServletPath() + "<br/>");
+		out.print("</body></html>");
+		out.close();
+	}
+
+}
